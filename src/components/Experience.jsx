7817ff12224806541792119f8496e582d23d771a
Ankit Mishra <ankit.mishra9780@gmail.com>
2023-05-16 07:33:09 +0530

    changes for resume button

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -105,7 +105,7 @@ function Experience() {
               target="_blank"
               href="/resume.pdf"
               download={true}
-              className="flex justify-between items-center w-full text-white"
+              className=""
             >
               <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
                 <div className="flex flex-row justify-evenly gap-5 ">
